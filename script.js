da35f79acf78c1d063ea3153dbe5caa4f5dee491
shahidhussain07 <sh604780@gmail.com>
2022-02-19 20:51:18 +0530

    added new features

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', function (e) {
     
     let addTxt = document.getElementById('addTxt');
+    let addTitle = document.getElementById('addTitle');
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         notesObj = [];
     }else{
         notesObj =JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value, 
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem('notes',JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     // console.log(notesObj);
     showNotes();
 })
@@ -31,8 +37,8 @@ function showNotes() {
         html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
-                    <p class="card-text">${element}</p>
+                    <h5 class="card-title">${index+1}. ${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
                     <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
             </div>  `;
@@ -42,7 +48,7 @@ function showNotes() {
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     }else{
-        notesElm.innerHTML =` Nothing to Show here.  Use "Add a Note" section above to add notes.  `
+        notesElm.innerHTML =`* Nothing to Show here! *`
     }
 
 }
